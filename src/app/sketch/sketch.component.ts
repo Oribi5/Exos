86ae8c7160bad26156cd80d5cb47f725e60bdc0e
Aidan Hughes <aidan.hughes44@gmail.com>
2021-04-06 23:05:16 +0100

    Adding 3D viewer

--- src/app/sketch/sketch.component.ts
+++ src/app/sketch/sketch.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+// import { OrbitControls } from 'three-orbit-controls'
 
 @Component({
   selector: 'sketch',
@@ -35,13 +37,15 @@ export class SketchComponent implements OnInit {
     let scene = new THREE.Scene();
 
     let camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
-    camera.position.z = 5;
+    camera.position.z = 50;
 
     let render = new THREE.WebGL1Renderer({antialias: true});
-    render.setClearColor("#e5e5e5");
+    // render.setClearColor("#00ff00");
     render.setSize(width, height);
     
     element.appendChild( render.domElement );
+
+    // const controls = new OrbitControls( camera, render.domElement );
 
     window.addEventListener('resize', () => {
       let {width, height} = this.getElementDimensions();
@@ -51,30 +55,63 @@ export class SketchComponent implements OnInit {
       camera.updateProjectionMatrix();
     });
 
-    let geometry = new THREE.BoxGeometry(1, 1 ,1);
-    let material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
+    const loader = new OBJLoader();
+
+    let geometry = new THREE.BoxGeometry(10, 10 ,10);
+    let material = new THREE.MeshLambertMaterial({color: 0xffffff});
     let mesh = new THREE.Mesh(geometry, material);
 
     mesh.position.x = -2;
 
     scene.add(mesh);
 
-    let light = new THREE.PointLight(0xFFFFFF, 1, 500);
-    light.position.set(10, 0, 25);
-    scene.add(light);
+    let keyLight = new THREE.DirectionalLight(new THREE.Color('hwl(30, 100%, 75%'), 1.0);
+    keyLight.position.set(-100, 0, 100);
+
+    let fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%'), 0.75);
+    fillLight.position.set(100, 0, 100);
+
+    let backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+    backLight.position.set(100, 0, -100).normalize();
+
+    scene.add(keyLight);
+    scene.add(fillLight);
+    scene.add(backLight);
 
     // let angle = 0;
 
     let animate = function() {
 
       // angle += 0.05;
-      mesh.rotation.y += 0.05;
+      mesh.rotation.y += 0.02;
 
       requestAnimationFrame(animate);
       render.render(scene, camera);
     }
 
     animate();
+
+    loader.load(
+      // resource URL
+      './assets/model/Final Asm - Forearm SubAsm-1 Spine-1.obj',
+      // called when resource is loaded
+      function ( object ) {
+        console.log("Load complete");
+        // object.traverse(function(child) {
+        //   if (child instanceof THREE.Mesh) {
+        //       child.material.color = 0xffb830;
+        //   }
+        // });
+        // object.position.set(0, 0, -53);
+        // object.materia
+        scene.add( object );
+
+      },
+      // called when loading is in progresses
+      ( xhr ) => console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ),
+      // called when loading has errors
+      ( error ) => console.log( 'An error happened' )
+    );
     
   }
 
